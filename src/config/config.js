7537aa8e0d3d4e329b3b7ba1cb1696fca3c38a27
Hasan Rahman <hasanrahman33605@gmail.com>
2023-11-21 15:33:10 +0600

    start

---
+++ src/config/config.js
@@ -0,0 +1,13 @@
+ const config ={
+
+    appWriteUri : import.meta.VITE_APPWRITE_URL,
+    appWriteProjectId : import.meta.VITE_APPWRITE_PROJECT_ID,
+    appWriteDatabaseId : import.meta.VITE_APPWRITE_DATABASE_ID,
+    appWriteCollectionId : import.meta.VITE_APPWRITE_COLLECTION_ID,
+    appWriteBucketId : import.meta.VITE_APPWRITE_BUCKET_ID,
+
+
+}
+
+export default config
+
